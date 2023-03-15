64910e67782767f87c440bb019ed8de0b9fb05e9
sg <sg@abc.com>
2023-03-15 22:46:50 +1100

    Refactored code

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	"github.com/hashicorp/go-hclog"
+	pb "github.com/sg83/grpc-currency-service/proto"
+	"github.com/sg83/grpc-currency-service/server"
+)
+
+func main() {
+	// Create a new gRPC server
+	log := hclog.Default()
+	gs := grpc.NewServer()
+	cs := server.NewCurrencyServer(log)
+
+	//Testing purpose - Disable later
+	reflection.Register(gs)
+
+	// Register the currencyServer as the implementation of the CurrencyServiceServer interface
+	pb.RegisterCurrencyServiceServer(gs, cs)
+
+	// Listen on port 50051
+	listener, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Error("Failed to listen: %v", err)
+	}
+
+	// Start the gRPC server
+	log.Info("Starting server on port %v", listener.Addr().String())
+	if err := gs.Serve(listener); err != nil {
+		log.Error("Failed to serve: %v", err)
+	}
+}
--- server/currency.go
+++ server/currency.go
@@ -1,46 +1,28 @@
-package main
+package server
 
 import (
-	"context"
-	"log"
-	"net"
-
-	"google.golang.org/grpc"
-
+	"github.com/hashicorp/go-hclog"
 	pb "github.com/sg83/grpc-currency-service/server/proto"
 )
 
 // Define a struct that implements the CurrencyServiceServer interface
-type currencyServer struct{}
+type currencyServer struct {
+	log hclog.Logger
+	pb.UnimplementedCurrencyServiceServer
+}
+
+func NewCurrencyServer(l hclog.Logger) *currencyServer {
+	return &currencyServer{l, pb.UnimplementedCurrencyServiceServer{}}
+}
 
 // Implement the GetExchangeRate method
 func (s *currencyServer) GetExchangeRate(ctx context.Context, req *pb.ExchangeRateRequest) (*pb.ExchangeRateResponse, error) {
 	// In a real implementation, you would retrieve the exchange rate data from some external API or database
 	// For this example, we'll just return a hardcoded exchange rate of 1.23
 	exchangeRate := 1.23
-
+	s.log.Info("Handle GetExchangeRate", "base:", req.BaseCurrency, "destination:", req.TargetCurrency)
 	// Create and return an ExchangeRateResponse message containing the exchange rate
 	return &pb.ExchangeRateResponse{
 		ExchangeRate: exchangeRate,
 	}, nil
-}
-
-func main() {
-	// Create a new gRPC server
-	server := grpc.NewServer()
-
-	// Register the currencyServer as the implementation of the CurrencyServiceServer interface
-	pb.RegisterCurrencyServiceServer(server, &currencyServer{})
-
-	// Listen on port 50051
-	listener, err := net.Listen("tcp", ":50051")
-	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
-	}
-
-	// Start the gRPC server
-	log.Printf("Starting server on port %v", listener.Addr().String())
-	if err := server.Serve(listener); err != nil {
-		log.Fatalf("Failed to serve: %v", err)
-	}
 }
